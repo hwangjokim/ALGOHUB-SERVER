4b918f1c35140d90125325be9fd11b5339233dcd
Kim EunSu <88280787+rladmstn@users.noreply.github.com>
2024-11-23 23:09:04 +0900

    refactor : 스터디 그룹 목록 조회 시 날짜 정보 Date 타입으로 수정 (#199)

--- src/main/java/com/gamzabat/algohub/feature/group/studygroup/dto/GetGroupResponse.java
+++ src/main/java/com/gamzabat/algohub/feature/group/studygroup/dto/GetGroupResponse.java
@@ -2,16 +2,14 @@ package com.gamzabat.algohub.feature.group.studygroup.dto;
 
 import java.time.LocalDate;
 
-import com.gamzabat.algohub.common.DateFormatUtil;
-
 import lombok.Getter;
 
 @Getter
 public class GetGroupResponse {
 	private final Long id;
 	private final String name;
-	private final String startDate;
-	private final String endDate;
+	private final LocalDate startDate;
+	private final LocalDate endDate;
 	private final String introduction;
 	private final String groupImage;
 	private final Boolean isOwner;
@@ -21,8 +19,8 @@ public class GetGroupResponse {
 		String groupImage, Boolean isOwner, String ownerNickname) {
 		this.id = id;
 		this.name = name;
-		this.startDate = DateFormatUtil.formatDate(startDate);
-		this.endDate = DateFormatUtil.formatDate(endDate);
+		this.startDate = startDate;
+		this.endDate = endDate;
 		this.introduction = introduction;
 		this.groupImage = groupImage;
 		this.isOwner = isOwner;
