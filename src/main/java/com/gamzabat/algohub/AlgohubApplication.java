50b84061a2bba7a742d67885993183dc02da9c27
HwangDo <96808859+hwangjokim@users.noreply.github.com>
2025-01-09 04:56:23 +0900

    env: Enforce the server's timezone to be set as Asia/Seoul (KST) (#273)

--- src/main/java/com/gamzabat/algohub/AlgohubApplication.java
+++ src/main/java/com/gamzabat/algohub/AlgohubApplication.java
@@ -1,5 +1,7 @@
 package com.gamzabat.algohub;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class AlgohubApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(AlgohubApplication.class, args);
 	}
 
