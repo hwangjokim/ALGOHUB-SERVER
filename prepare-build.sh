fd75c1035a987dd8fbf0fce9911755eba80272db
HwangDo <96808859+hwangjokim@users.noreply.github.com>
2025-01-16 00:24:41 +0900

    env : Fix production build error (#288)
    * env : Fix production build error
    
    * env : Fix production build error

--- prepare-build.sh
+++ prepare-build.sh
@@ -10,8 +10,8 @@ is_empty_or_null() {
   [ -z "$1" ] || [ "$1" = "null" ]
 }
 
-if is_empty_or_null "$DEV_NAME" || is_empty_or_null "$CREDENTIAL_NAME" || is_empty_or_null "$CREDENTIAL_PW"; then
-  echo "필요한 환경변수(DEV_NAME, CREDENTIAL_NAME, CREDENTIAL_PW) 중 하나 이상이 설정되어 있지 않거나 null입니다."
+if is_empty_or_null "$CREDENTIAL_NAME" || is_empty_or_null "$CREDENTIAL_PW"; then
+  echo "필요한 환경변수(CREDENTIAL_NAME, CREDENTIAL_PW) 중 하나 이상이 설정되어 있지 않거나 null입니다."
   exit 1
 fi
 download_yml() {
