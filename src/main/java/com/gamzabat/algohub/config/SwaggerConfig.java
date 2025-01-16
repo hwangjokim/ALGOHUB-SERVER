f0626ac2fcf7362f4c87190c903eb3926bc2da40
HwangDo <96808859+hwangjokim@users.noreply.github.com>
2025-01-16 23:22:02 +0900

    env : Apply https for swagger (#291)

--- src/main/java/com/gamzabat/algohub/config/SwaggerConfig.java
+++ src/main/java/com/gamzabat/algohub/config/SwaggerConfig.java
@@ -1,18 +1,27 @@
 package com.gamzabat.algohub.config;
 
+import java.util.Arrays;
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.gamzabat.algohub.constants.ApiConstants;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
+
+	@Value("${server.port}")
+	private String serverPort;
+
 	@Bean
 	public OpenAPI openAPI() {
 		SecurityScheme scheme = new SecurityScheme()
@@ -20,12 +29,22 @@ public class SwaggerConfig {
 			.in(SecurityScheme.In.HEADER).name("Authorization");
 		SecurityRequirement requirement = new SecurityRequirement().addList("bearerAuth");
 
+		Server prodServer = new Server();
+		prodServer.setDescription("Algohub API");
+		prodServer.setUrl(ApiConstants.SERVER_HTTPS_ENDPOINT);
+
+		Server localServer = new Server();
+		localServer.setDescription("Algohub API for LOCAL");
+		localServer.setUrl("http://localhost:" + serverPort);
+
 		return new OpenAPI()
 			.components(new Components().addSecuritySchemes("bearerAuth", scheme))
 			.security(Collections.singletonList(requirement))
 			.info(new Info()
 				.title("AlgoHub API 명세서")
 				.description("AlgoHub API 명세서 입니다.")
-				.version("1.0.0"));
+				.version("1.0.0"))
+			.servers(Arrays.asList(localServer, prodServer));
+
 	}
 }
--- src/main/java/com/gamzabat/algohub/constants/ApiConstants.java
+++ src/main/java/com/gamzabat/algohub/constants/ApiConstants.java
@@ -4,6 +4,7 @@ public final class ApiConstants {
 	public static final String SOLVED_AC_PROBLEM_API_URL = "https://solved.ac/api/v3/problem/lookup?problemIds=";
 	public static final String BOJ_USER_PROFILE_URL = "https://www.acmicpc.net/user/";
 	public static final String BOJ_PROBLEM_URL = "www.acmicpc.net";
+	public static final String SERVER_HTTPS_ENDPOINT = "https://api.algohub.kr";
 
 	public static final String GITHUB_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
 	public static final String GITHUB_USER_URL = "https://api.github.com/user";
