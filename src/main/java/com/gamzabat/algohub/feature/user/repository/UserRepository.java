f1c0bee2a07f6352bd988dc0ecfad3781d862223
Kim Eunsu <88280787+rladmstn@users.noreply.github.com>
2025-04-13 20:43:05 +0900

    fix: 회원 이메일 존재 오류 수정 (#372)

--- src/main/java/com/gamzabat/algohub/feature/user/repository/UserRepository.java
+++ src/main/java/com/gamzabat/algohub/feature/user/repository/UserRepository.java
@@ -3,21 +3,24 @@ package com.gamzabat.algohub.feature.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.gamzabat.algohub.feature.user.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	@Query("SELECT EXISTS ("
+		+ "SELECT 1 FROM User u "
+		+ "WHERE u.email = :email "
+		+ "AND u.deletedAt IS NULL"
+		+ ")")
 	boolean existsByEmail(String email);
 
+	@Query("SELECT u FROM User u "
+		+ "WHERE u.email = :email "
+		+ "AND u.deletedAt IS NULL")
 	Optional<User> findByEmail(String email);
 
-	void deleteByEmail(String email);
-
 	Optional<User> findByBjNickname(String bjNickname);
-
-	Optional<User> findById(Long Id);
-
-	boolean existsByBjNickname(String bjNickname);
 
 	boolean existsByNickname(String nickname);
 
